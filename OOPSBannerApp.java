28d11ddd0283f508ce64575e7484c605f7b07071
Ishaan <is2350@srmist.edu.in>
2026-04-16 11:25:28 +0530

    feature/UC6: Banner using functions (modular design)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,49 @@
 public class OOPSBannerApp {
+
+    public static String getO() {
+        return String.join("\n",
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****"
+        );
+    }
+
+    public static String getP() {
+        return String.join("\n",
+                "****",
+                "*   *",
+                "*   *",
+                "****",
+                "*",
+                "*",
+                "*"
+        );
+    }
+
+    public static String getS() {
+        return String.join("\n",
+                "*****",
+                "*",
+                "*",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        );
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", "*****", "*****", "****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*"),
-            String.join(" ", "*   *", "*   *", "*   *", "*"),
-            String.join(" ", "*   *", "*   *", "****", "*****"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", "*****", "*****", "*", "*****")
-        };
+        String[] o = getO().split("\n");
+        String[] p = getP().split("\n");
+        String[] s = getS().split("\n");
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
     }
 }
